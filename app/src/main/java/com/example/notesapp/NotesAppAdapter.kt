3dfda148a6a790837b97f9d9f8ae89ecb01db74c
emirk <emir.karabey1@hotmail.com>
2022-02-14 20:08:33 +0300

    Create recycler view

---
+++ app/src/main/java/com/example/notesapp/NotesAppAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.notesapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.databinding.RecyclerRowBinding
+
+class NotesAppAdapter(var notesList: ArrayList<String>): RecyclerView.Adapter<NotesAppAdapter.NotesHolder>() {
+
+    class NotesHolder(val binding: RecyclerRowBinding):RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesHolder {
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return NotesHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NotesHolder, position: Int) {
+        holder.binding.recyclerRowTextView.text = notesList.get(position).toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return notesList.count()
+    }
+
+}
